8be5532867640966e31a60b5135a26abca25ac36
Ayush755 <olibhfvg@gmail.com>
2026-07-03 16:17:07 +0530

    Python lang upg

--- Ayush_Python.py
+++ Ayush_Python.py
@@ -1,14 +1,69 @@
 # to print anything
 print('Hellow ayush')
 #to print multiple line
-print("""
-                    name : Ayush
-                    age:20
-                    loves: Dawg""")
+print("""name : Ayush age:20 loves:SJ""")
 #to assignment a value to a variable
 a=10
 print(a)
 #to take input from the user
 name= input('enter name:')
 print('hellow', name)
-
+#to sum and usska datatypes and usska conversion | in 'b' 1.1(float) is converted into integer jo ki h '1'
+a=10
+integer_number=2
+float_number=1.1
+b=int(float_number)
+sum = a + integer_number + float_number + int(float_number)
+print("sum ayega :", sum)
+#sum(+) operator
+a= 10
+b= a+ 5
+sum = a+b
+print('sum ayega:', sum)
+#diff(-) operator
+a=2.4
+b=int(a)
+diff= a-b
+print('difference ayega', diff)
+#multiplication(*) operator
+a=6.7
+b=float(a)
+product=a*b
+print("product ayega:" , product)
+#division(/) operator eg- 6.7/6.7
+div=a/b
+print('division ayega:', div)
+#quostient(//) operator same example jo ki uper h
+questient=a//b
+print('questient ayega:',questient)
+#remainder(%) operator same example jo ki uper h
+remainder=a%b
+print("remainder ayega", remainder)
+#raise to power (**) operator
+a=2
+b=3
+cube= a**b
+print('2 ka cube ayega:',cube)
+#Multipilcation se ek string ko repeat v kar sake h
+a= "FAHHHH!!!"
+b= a*3
+print(b)
+#Assignment operator (+=,-=,*=,/=,//=,**=)
+x=10
+x+=2 #x=x+2
+print(x)
+y=2
+y-=1 #y=y-1
+print(y)
+z=2
+z*=2 #z=2*2
+print(z)
+m=10
+m/=2 #m/2
+print(m)
+n=11
+n//=2 #questient
+print(n)
+k=5
+k%=2 #remainder
+print(k)

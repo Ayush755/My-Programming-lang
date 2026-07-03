3e352910ad6b80935ad2ab0776f9cf271840c873
Ayush755 <olibhfvg@gmail.com>
2026-07-03 14:48:26 +0530

    Python lang

---
+++ Ayush_Python.py
@@ -0,0 +1,14 @@
+# to print anything
+print('Hellow ayush')
+#to print multiple line
+print("""
+                    name : Ayush
+                    age:20
+                    loves: Dawg""")
+#to assignment a value to a variable
+a=10
+print(a)
+#to take input from the user
+name= input('enter name:')
+print('hellow', name)
+
